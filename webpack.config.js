3d7f8fcd11679bcfc5203ae79eeffb742faa97e7
karan100010 <karanjoshi1993@gmail.com>
2022-11-26 11:43:40 +0530

    basic infra

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,53 @@
 const path = require('path')
 const isProduction = process.env.NODE_ENV == 'production'
 
-const config = {
-  entry: './built-tsc/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'viz.js',
-  }
-}
+// const config = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'viz.js',
+//  //use config for react  and react-dom
 
-module.exports = () => {
-  config.mode = isProduction ? "production" : "development"
-  return config
-}
+    
+//   },
+//   module: {
+//     test: /\.jsx?$/,
+//     loader: 'babel-loader',
+//     exclude: /node_modules/,
+//     query: {
+//         presets: ['es2015']
+//     }
+// }
+ 
+
+
+// }
+
+// module.exports = () => {
+//   config.mode = isProduction ? "production" : "development"
+//   return config
+// }
+
+module.exports = {
+  entry: {
+      app: './src/index.jsx',
+      vendor: ["react","react-dom"]
+      
+  },
+  mode: 'development',
+  output: {
+      filename: 'dist/[name].js',
+      path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+      rules: [{
+          test: /\.jsx?$/,
+        
+          use: {
+              loader: 'babel-loader?cacheDirectory=true',
+          }
+      }]
+    
+  }
+  
+};
