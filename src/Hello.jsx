c11f7076b7ba0ab74ff5ac7d2270d21ffe2b5d96
kunaaalll <67854709+kunaaalll@users.noreply.github.com>
2022-11-30 21:25:12 -0800

    initial commit

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,9 +1,18 @@
 import { RowEntry } from "@google/dscc"
 
+import "./style.css"
+
+
 export function Hello() {
   return <h1>Hello mr dj</h1>
 }
-
-export function Num(num ){
-  return <h1>{num}</h1>
+//make a function return h1 with pink color and balck background
+export function Num( num) { 
+  return <h1 style={{color:"pink",backgroundColor:"black"}}>{num}</h1>
 }
+
+
+// export function Num(num ){
+//   return <h1>
+//     {num}</h1>
+// }
